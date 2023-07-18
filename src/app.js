9ccf53b6fe1dc7f42702c500603203563613ae7f
MeganKullu <kullumegan@gmail.com>
2023-07-18 10:14:39 +0300

    updated README file

--- src/app.js
+++ src/app.js
@@ -11,141 +11,123 @@ var currentDay = todaysDate.getDate();
 
 // Function to calculate age
 function calculateAge() {
-    var dayCalc = document.getElementById('day').value;
-    var monthCalc = document.getElementById('month').value;
-    var yearCalc = document.getElementById('year').value;
-  
-    var dayValid = validateDay(parseInt(dayCalc), parseInt(monthCalc), parseInt(yearCalc));
-    var monthValid = validateMonth(parseInt(monthCalc));
-    var yearValid = validateYear();
-  
-    if (dayValid && monthValid && yearValid) {
-      var dayAge = Math.abs(currentDay - parseInt(dayCalc));
-      var monthAge = Math.abs(currentMonth - parseInt(monthCalc));
-      var yearAge = currentYear - parseInt(yearCalc);
-  
-      document.getElementById('yearsCalc').innerHTML = isNaN(yearAge) ? '--' : yearAge;
-      document.getElementById('monthsCalc').innerHTML = isNaN(monthAge) ? '--' : monthAge;
-      document.getElementById('daysCalc').innerHTML = isNaN(dayAge) ? '--' : dayAge;
-  
-      document.getElementById('yearsCalc').classList.remove('error');
-      document.getElementById('monthsCalc').classList.remove('error');
-      document.getElementById('daysCalc').classList.remove('error');
-    } else {
-      document.getElementById('yearsCalc').innerHTML = '--';
-      document.getElementById('monthsCalc').innerHTML = '--';
-      document.getElementById('daysCalc').innerHTML = '--';
-  
-      if (!dayValid) {
-        document.getElementById('daysCalc').innerHTML = '--';
-      }
-  
-      document.getElementById('yearsCalc').classList.add('error');
-      document.getElementById('monthsCalc').classList.add('error');
-      document.getElementById('daysCalc').classList.add('error');
-    }
+  var dayCalc = document.getElementById('day').value;
+  var monthCalc = document.getElementById('month').value;
+  var yearCalc = document.getElementById('year').value;
+
+  var dayValid = validateDay(parseInt(dayCalc), parseInt(monthCalc), parseInt(yearCalc));
+  var monthValid = validateMonth(parseInt(monthCalc));
+  var yearValid = validateYear();
+
+  if (dayValid && monthValid && yearValid) {
+    var dayAge = Math.abs(currentDay - parseInt(dayCalc));
+    var monthAge = Math.abs(currentMonth - parseInt(monthCalc));
+    var yearAge = currentYear - parseInt(yearCalc);
+
+    document.getElementById('yearsCalc').innerHTML = isNaN(yearAge) ? '--' : yearAge;
+    document.getElementById('monthsCalc').innerHTML = isNaN(monthAge) ? '--' : monthAge;
+    document.getElementById('daysCalc').innerHTML = isNaN(dayAge) ? '--' : dayAge;
+
+    resetBorderAndTextColor('day');
+    resetBorderAndTextColor('month');
+    resetBorderAndTextColor('year');
+  } else {
+    document.getElementById('yearsCalc').innerHTML = '--';
+    document.getElementById('monthsCalc').innerHTML = '--';
+    document.getElementById('daysCalc').innerHTML = '--';
+
+    setBorderAndTextColor('day', !dayValid);
+    setBorderAndTextColor('month', !monthValid);
+    setBorderAndTextColor('year', !yearValid);
+  }
 }
-  
+
 // Leap year calculation
 function leapYear(year) {
-    if (year === undefined) {
-      return false;
-    }
-  else return year % 100 === 0 ? year % 400 === 0 : year % 4 === 0;
+  if (year === undefined) {
+    return false;
+  } else {
+    return year % 100 === 0 ? year % 400 === 0 : year % 4 === 0;
+  }
 }
 
 // Day validation based on month and year
 function validateDay(day, month, year) {
-    var dayError = document.getElementById('dayError');
+  var dayError = document.getElementById('dayError');
+  var maxDays = 31;
 
-    
-    var monthsWith31Days = [1, 3, 5, 7, 8, 10, 12];
-    var monthsWith30Days = [4, 6, 9, 11];
-
-    var maxDays = 31;
-
-    
-
-
-    if (parseInt(day) < 1 || parseInt(day) > maxDays) {
-        dayError.innerHTML = 'Must be a valid day';
-        return false;
-    }
-    
-    if (month === 2) {
-        if (leapYear(year)) {
-          if (day > 29) {
-            dayError.innerHTML = 'Feb has 29 days in this year';
-            return false;
-          }
-        } 
-    }
-
-
-    if (month === 2) {
-      
-        if (year !== true) {
-          if (day === 29 && (!leapYear(year))) {
-            dayError.innerHTML = 'Feb has 28 days in this year';
-            return false;
-          }
-
-          else if (day === 29 && year == "") {
-            dayError.innerHTML = '';
-          }
-          else dayError.innerHTML = "" ;
-        }
-          
-    }
-
-
-
-    if (monthsWith30Days.includes(month) && day > 30) {
-        dayError.innerHTML = 'This month has 30 days';
-        return false;
-    }
-
-    if (monthsWith31Days.includes(month) && day > 31) {
-        dayError.innerHTML = 'This month has 31 days';
-        return false;
-    }
-
-
-   return true;
+  if (parseInt(day) < 1 || parseInt(day) > maxDays) {
+    dayError.innerHTML = 'Must be a valid day';
+    return false;
   }
+
+  if (month === 2) {
+    if (leapYear(parseInt(year))) {
+      if (day > 29) {
+        dayError.innerHTML = 'Feb has 29 days in this year';
+        return false;
+      }
+    } else {
+      if (day > 28) {
+        dayError.innerHTML = 'Feb has 28 days in this year';
+        return false;
+      }
+    }
+  }
+
+  dayError.innerHTML = '';
+  return true;
+}
 
 // Month validation
 function validateMonth(month) {
-    var monthError = document.getElementById('monthError');
+  var monthError = document.getElementById('monthError');
 
-    if (parseInt(month) < 1 || parseInt(month) > 12) {
-        monthError.innerHTML = 'Must be a valid month';
-        return false;
-    }
+  if (parseInt(month) < 1 || parseInt(month) > 12) {
+    monthError.innerHTML = 'Must be a valid month';
+    return false;
+  }
 
-    monthError.innerHTML = '';
-
-    return validateDay(parseInt(month), parseInt(document.getElementById('year').value), year);
+  monthError.innerHTML = '';
+  return true;
 }
 
 // Year validation
 function validateYear() {
-    var year = document.getElementById('year').value;
-    var yearError = document.getElementById('yearError');
+  var year = document.getElementById('year').value;
+  var yearError = document.getElementById('yearError');
 
-    if (parseInt(year) > currentYear) {
-        yearError.innerHTML = 'Must be in the past';
-        return false;
-    }
+  if (parseInt(year) > currentYear) {
+    yearError.innerHTML = 'Must be in the past';
+    return false;
+  }
 
-    if (parseInt(year) < 1900) {
-        yearError.innerHTML = 'Are you alive?';
-        year.style.border = 'border-light-red';
-        return false;
-    }
+  if (parseInt(year) < 1900) {
+    yearError.innerHTML = 'Are you alive?';
+    return false;
+  }
 
-    yearError.innerHTML = '';
-    return true;
+  yearError.innerHTML = '';
+  return true;
+}
+
+
+// Set border color and label text color for input element
+function setBorderAndTextColor(elementId, isInvalid) {
+  var inputElement = document.getElementById(elementId);
+  var labelElement = document.querySelector('label[for="' + elementId + '"]');
+
+  inputElement.style.borderColor = isInvalid ? '#FF5757' : '#DBDBDB';
+  labelElement.style.color = isInvalid ? '#FF5757' : '#716F6F';
+}
+
+// Reset border color and label text color for input element
+function resetBorderAndTextColor(elementId) {
+  var inputElement = document.getElementById(elementId);
+  var labelElement = document.querySelector('label[for="' + elementId + '"]');
+
+  inputElement.style.borderColor = '#DBDBDB';
+  labelElement.style.color = '#716F6F';
 }
 
 // Add event listeners to trigger calculateAge function
